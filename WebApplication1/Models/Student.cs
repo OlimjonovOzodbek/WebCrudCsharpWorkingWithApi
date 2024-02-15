f881b1eb6966287cf433f871179a057c861e0555
Ozodbek <olimjanovozodbek32@gmail.com>
2024-02-15 19:23:01 +0500

    Add project files.

---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string full_name { get; set; }
+        public int age { get; set; }
+        public int student_group { get; set; }
+        public string phone { get; set; }
+    }
+}
+
+//student_id full_name age student_group phone
---
+++ WebApplication1/ModelsTDO/Student_TDO.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.ModelsTDO
+{
+    public class Student_TDO
+    {
+        public string full_name { get; set; }
+        public int age { get; set; }
+        public int student_group { get; set; }
+        public string phone { get; set; }
+    }
+}
